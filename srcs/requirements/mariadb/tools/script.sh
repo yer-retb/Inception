642551cf1f2f9d26fdf425e7d779e45ebc7f4597
yer-retb <youssef1erretby10@gmail.com>
2023-09-20 20:52:17 +0100

    mariadb finished

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 service mariadb start 
-mariadb -u root -e "CREATE DATABASE 'test';"
-mariadb -u root -e "CREATE USER 'usef'@'localhost' IDENTIFIED BY '1234' ; GRANT ALL PRIVILEGES ON *.* TO 'usef'@'localhost' ; FLUSH PRIVILEGES; "
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '1234'; FLUSH PRIVILEGES;"
 
-exec $@ 
+mariadb -u root -e "CREATE USER IF NOT EXISTS 'usef'@'%' IDENTIFIED BY '1234';"
+
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS test;"
+
+mariadb -u root -e "GRANT ALL PRIVILEGES ON test.* to 'usef'@'%';"
+
+mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '1234'; "
+
+mariadb -u root -e "FLUSH PRIVILEGES;"
