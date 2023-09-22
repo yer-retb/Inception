57eed09ae03c462e36719b6383d95ecd94f7ffe8
yer-retb <youssef1erretby10@gmail.com>
2023-09-22 19:15:39 +0100

    docker compose

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "listen=0.0.0.0:9000" >> /etc/php/7.4/fpm/pool.d/www.conf
-
 wp core download
 
-wp config create --dbname=wordpress --dbuser=user --dbpass=password --dbhost=localhost --dbprefix=wp_
+wp config create --dbname=wordpress --dbuser=user --dbpass=password --dbhost=localhost --dbprefix=wp_ --allow-root
 
-wp core install --url=yourdomain.com --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
+wp core install --url=yourdomain.com --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
 
-wp user create user user@example.com --role=author
+wp user create user user@example.com --role=author --allow-root
+
+exec "$@"
