a0c01efa8343418778176d7eccf32e402b2bf113
yer-retb <youssef1erretby10@gmail.com>
2023-09-28 03:20:26 +0100

    wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-service mariadb start 
+service mariadb start
 
-mariadb -u root -e "CREATE USER IF NOT EXISTS 'usef'@'%' IDENTIFIED BY '1234';"
+mariadb -h localhost -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS test;"
+mariadb -h localhost -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -u root -e "GRANT ALL PRIVILEGES ON test.* to 'usef'@'%';"
+mariadb -h localhost -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '1234'; "
+# mariadb -h localhost -e "ALTER USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
-mariadb -u root -e "FLUSH PRIVILEGES;"
+mariadb -h localhost -e "FLUSH PRIVILEGES;"
+
+echo "Database and user created successfully!"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-wp core download
+echo "listen=9000" >>  /etc/php/7.4/fpm/pool.d/www.conf
 
-wp config create --dbname=wordpress --dbuser=user --dbpass=password --dbhost=localhost --dbprefix=wp_ --allow-root
+wp core download --allow-root
 
-wp core install --url=yourdomain.com --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com --allow-root
+wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${MYSQL_HOST} --allow-root
 
-wp user create user user@example.com --role=author --allow-root
+wp core install --url=${WP_URL} --title=${WP_TITEL} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
 
-exec "$@"
+
+wp user create ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PW} --allow-root
+
+php-fpm7.4 -R -F
